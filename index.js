adecc968cad8ab8b22bd2b87d13be3e985d29216
what? <jack.weller@hotmail.com>
2022-12-05 17:13:01 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 fetch("./sites.json")
-.then(response => {
-   return response.json();
-})
-.then(list => 
-    for(let i = 0; i < list.lenght; i++) {
+.then(response => {return response.json();})
+.then(data => 
+    data.forEach(site => {
         const div = document.createElement('div');
         const status = "";
 
-        if(list[i].info.blocked == false) {
+        if(site[i].info.blocked == false) {
             status = "Unblocked";
-        } if(list[i].info.blocked == true) {
+        } if(site[i].info.blocked == true) {
             status = "Blocked";
-        } if(list[i].info.risk == true) {
+        } if(site[i].info.risk == true) {
             status += " <b>Use at own risk</b>"
         }
 
         div.className = 'web';
     
         div.innerHTML = `
-        <h2 class="web-text">${list[i]}</h2>
+        <h2 class="web-text">${site[i]}</h2>
         <div>
-            <h3 class="web-text"><a href="${list[i].link}" target="_blank">${list[i]} link</a></h3>
+            <h3 class="web-text"><a href="${site[i].link}" target="_blank">${site[i]} link</a></h3>
             <p class="web-text">
                 Status - ${status}
-                Class - ${list[i].info.class}
+                Class - ${site[i].info.class}
             </p>
         </div>
         `;
     
         document.getElementById('content').appendChild(div);
-        setTimeout(500, function(){console.log("Loaded: " + list[i])})
+        setTimeout(250, function(){console.log("Loaded: " + site[i])})
     }
-}
+));
